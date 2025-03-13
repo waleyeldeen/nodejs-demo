f2190bf5697626654270446340f7dcc11cee1534
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-03-13 22:44:57 +0200

    certs

--- util/database.js
+++ util/database.js
@@ -1,25 +1,39 @@
 const Sequelize = require("sequelize"); //sequelize is used for session-store
 const mysql = require("mysql2");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const sequelize = new Sequelize(
-    process.env.MYSQLDEFAULTDATABASE,
-    process.env.MYSQLUSER,
-    process.env.MYSQLPASSWORD,
-    {
-        host: process.env.MYSQLHOST,
-        dialect: "mysql",
-        logging: false,
-    }
+  process.env.MYSQLDEFAULTDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    dialect: "mysql",
+    logging: false,
+    ...(isProduction && {
+      dialectOptions: {
+        ssl: {
+          ca: Buffer.from(process.env.DBCERT, "base64").toString("utf-8"),
+        },
+      },
+    }),
+  }
 );
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDEFAULTDATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDEFAULTDATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ...(isProduction && {
+    ssl: {
+      ca: Buffer.from(process.env.DBCERT, "base64").toString("utf-8"),
+    },
+  }),
 });
 
 exports.promisePool = pool.promise();
